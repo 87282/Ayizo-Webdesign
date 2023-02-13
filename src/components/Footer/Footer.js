b73735fb34a106de80ed4b78e6eae574d42fc135
Lars Vermeulen <Lars@MacBook-Air-van-Bente.local>
2023-02-13 12:09:21 +0100

    ayizoweb

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,17 @@ import {
   SocialIconsContainer
 } from './FooterStyles';
 import SocialMedia from "../SocialMedia/SocialMedia.component";
+import { SectionText, SectionTitle } from '../../styles/GlobalComponents';
 
 const Footer = () => {
   return (
- <></>
+    <FooterWrapper>
+      <SectionTitle>Contact ons</SectionTitle>
+      <SectionText>U kont ons contacteren via<br/> +31619957856 of <br/>0619957856</SectionText>
+      <SocialIconsContainer>
+        <SocialMedia inFooter/>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
+import Footer from '../components/Footer/Footer';
 
 const Home = () => {
   return (
